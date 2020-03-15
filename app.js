ee20150baeabb9bd19b1a94bb32bd9ce6b231179
Guy Meiri <guymeiri312@gmail.com>
2020-03-15 20:19:32 +0200

    dice roll works!

--- app.js
+++ app.js
@@ -8,3 +8,41 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click',
+    function () {
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';;
+    
+    
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        document.querySelector(`#current-${activePlayer}`).textContent = dice;
+
+        if (dice !== 1) {
+            // var currScore = parseInt(document.querySelector(`#score${activePlayer}`).);
+
+
+            // document.querySelector(`#score${activePlayer}`).textContent = currScore + dice;
+        }
+    });
+
+function btn() {
+    dice = Math.floor(Math.random() * 6) + 1;
+}
